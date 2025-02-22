5433eb0eccfbae740eca4fde1b0c29715da51400
Willian Chiquinato <willianchiquinato@hotmail.com>
2025-02-22 13:57:56 -0300

    tela carros e header

---
+++ JS/js.js
@@ -0,0 +1,10 @@
+function loadHeader() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Erro ao carregar o header:', error));
+}
+
+window.onload = loadHeader;
