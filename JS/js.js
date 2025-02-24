2b78f8e76694ce464bbea37fa43eb4cadf996fab
WillianChiquinato <willianchiquinato@hotmail.com>
2025-02-24 16:22:42 -0300

    Header & Funcionarios

--- JS/js.js
+++ JS/js.js
@@ -1,10 +1,37 @@
-function loadHeader() {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header').innerHTML = data;
-        })
-        .catch(error => console.error('Erro ao carregar o header:', error));
-}
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
 
-window.onload = loadHeader;
+        // Após carregar o header, ativar o link correto
+        const activePage = window.location.pathname;
+        console.log(activePage);
+
+        if (activePage.endsWith("/index.html") || activePage === "/") {
+            const inicioHeader = document.getElementById("inicioHeader");
+            if (inicioHeader) {
+                inicioHeader.classList.add("active");
+            } else {
+                console.error("Elemento com ID 'inicioHeader' não encontrado.");
+            }
+        }
+
+        if (activePage.endsWith("/carros.html") || activePage === "/") {
+            const carrosHeader = document.getElementById("carrosHeader");
+            if (carrosHeader) {
+                carrosHeader.classList.add("active");
+            } else {
+                console.error("Elemento com ID 'inicioHeader' não encontrado.");
+            }
+        }
+
+        if (activePage.endsWith("/funcionarios.html") || activePage === "/") {
+            const funcionariosHeader = document.getElementById("funcionariosHeader");
+            if (funcionariosHeader) {
+                funcionariosHeader.classList.add("active");
+            } else {
+                console.error("Elemento com ID 'inicioHeader' não encontrado.");
+            }
+        }
+    })
+    .catch(error => console.error('Erro ao carregar o header:', error));
